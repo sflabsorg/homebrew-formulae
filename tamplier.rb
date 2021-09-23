e81bc0886834fbeaadd9cf91bd459bb886520dd4
Anton <oboupo@gmail.com>
2021-09-23 23:35:06 +0300

    Bump version

--- tamplier.rb
+++ tamplier.rb
@@ -2,7 +2,7 @@ class Tamplier < Formula
   desc "A utility to perfrom iOS project & API generation for SF Labs."
   homepage "sflabs.org"
   url "git@github.com:sflabsorg/tamplier.git", :using => :git
-  version "0.0.4"
+  version "0.0.5"
 
   depends_on xcode: ["12.0", :build]
 
